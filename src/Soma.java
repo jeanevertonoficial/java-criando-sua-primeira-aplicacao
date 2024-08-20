10135d082d0eadc7956d5f4c6724e7b58841c7ff
Jean Everton <52806219+jeanevertonoficial@users.noreply.github.com>
2024-08-19 21:58:14 -0300

    #Formaçãojava - 1° curso: criando sua primeira aplicação java (inicio).

--- src/Soma.java
+++ src/Soma.java
@@ -4,13 +4,13 @@ public class Soma {
         subtracao();
     }
 
-    private static void soma() {
+    public static void soma() {
         int num1 = 10;
         int num2 = 5;
         System.out.println("A soma de " + num1 +"+"+ num2 + " = " + (num1 + num2));
     }
 
-    private static void subtracao() {
+    public static void subtracao() {
         int num1 = 10;
         int num2 = 5;
         System.out.println("A subtração de " + num1 +"-"+ num2 + " = " + (num1 - num2));
